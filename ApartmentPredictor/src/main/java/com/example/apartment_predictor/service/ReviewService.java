5845f1983ca7b18a9063d9e43f86e27663eef6e0
nil.sanca97 <nil.sanca97@gmail.com>
2025-12-11 12:14:07 +0100

    feat: Put ReviewService inside Service Package. At the moment in this class I only put operations CRUD: CREATE (reviews)

---
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/service/ReviewService.java
@@ -0,0 +1,8 @@
+package com.example.apartment_predictor.service;
+
+import com.example.apartment_predictor.model.Apartment;
+
+public class ReviewService {
+
+    public void createReview (Apartment apartment) {}
+}
