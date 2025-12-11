8199c9575d9c28905971f70c0aacdea0b233e1d4
nil.sanca97 <nil.sanca97@gmail.com>
2025-12-11 12:12:12 +0100

    feat: create Service package with ApartmentService inside. At the moment in this class I only put operations CRUD (that I can do thanks to interface "ApartmentRepository", who allows to use (extends) crudRepository class (from JAVA).

---
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/service/ApartmentService.java
@@ -0,0 +1,33 @@
+package com.example.apartment_predictor.service;
+
+import com.example.apartment_predictor.model.Apartment;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ApartmentService {
+
+    /*// implement CRUD operations: read
+    public List<Apartment> findAll() {
+
+        return null;
+    }
+
+    // implement CRUD operations: CREATE
+    public void createApartment(){}
+
+    implement CRUD operations: UPDATE
+    public void updateApartment (){}
+
+    implement CRUD operations: DELETE
+    public void deleteApartment (){}
+
+    implement CRUD operations: READ (read "Apartment" by Id)
+    public void findApartmentById (){}
+
+    implement CRUD operations: READ (read "Apartment" by Price)
+    public void findApartmentByPrice (){}
+    */
+
+}
