2dbfc93e75755a39b052d3f4c2bd5be62ef2c4c9
nil.sanca97 <nil.sanca97@gmail.com>
2026-01-07 13:33:01 +0100

    TEST: testInsertReviewIntoApartment & testInsertAndReadReviewIntoApartment in ApartmentTest

--- ApartmentPredictor/src/test/java/com/example/apartment_predictor/ApartmentTests.java
+++ ApartmentPredictor/src/test/java/com/example/apartment_predictor/ApartmentTests.java
@@ -1,16 +1,27 @@
 package com.example.apartment_predictor;
 
 import com.example.apartment_predictor.model.Apartment;
+import com.example.apartment_predictor.model.Review;
 import com.example.apartment_predictor.repository.ApartmentRepository;
+import com.example.apartment_predictor.repository.ReviewRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ApartmentTests {
 
     @Autowired
     ApartmentRepository apartmentRepository;
+
+    @Autowired
+    ReviewRepository reviewRepository;
 
     @Test
     void testCreateApartment() {
@@ -52,6 +63,7 @@ public class ApartmentTests {
         System.out.println("Apartment 1: " + apartment1);
         System.out.println("\nApartment 2: " + apartment2);
         System.out.println("\nApartment 3: " + apartment3);
+        System.out.println("\nApartment 4: " + apartment4);
 
         apartmentRepository.save(apartment1);
         apartmentRepository.save(apartment2);
@@ -65,9 +77,106 @@ public class ApartmentTests {
             System.out.println("#" + index);
             System.out.println(apartment);
         }
-
         //apartmentRepository.findAll().forEach(System.out::println);
 
+    }
+    @Test
+    void testInsertReviewIntoApartment(){
+        Apartment apartment1 = new Apartment();
+        apartment1.setPrice(300000L);
+        // Display the created apartment1
+        System.out.println("\n=== Created Apartment1 ===");
+        System.out.println("Apartment 1: " + apartment1);
+
+        apartmentRepository.save(apartment1);
+
+        Review review1 = new Review();
+        review1.setTitle("Fantastic house for enjoy a holidays");
+        review1.setApartment(apartment1);
+
+        Review review2 = new Review();
+        review2.setTitle("Lovely house in the middle of Barcelona town");
+        review2.setApartment(apartment1);
+
+        Review review3 = new Review();
+        review3.setTitle("Great value and lovely host");
+        review3.setApartment(apartment1);
+
+        // Display the created reviews
+        System.out.println("\n=== Created Reviews ===");
+        System.out.println("Review 1: " + review1);
+        System.out.println("Review 2: " + review2);
+        System.out.println("Review 3: " + review3);
+
+        reviewRepository.save(review1);
+        reviewRepository.save(review2);
+        reviewRepository.save(review3);
+
+        int index = 0;
+        System.out.println("\n=== Reviews in the Database ===");
+        for (Review review : reviewRepository.findAll()){
+            index++;
+            System.out.println("#" + index);
+            System.out.println(review);
+        }
+    }
+
+    @Test //para ver (comprobar) las reviews de un apartamento desde JPA (backend)
+    @Transactional // because I have LAZY in @OneToMany (from apartment class) in attribute List<Review> reviews
+    void testInsertAndReadReviewIntoApartment() {
+
+        //1. Create an apartment
+        Apartment apartment1 = new Apartment();
+        apartment1.setPrice(300000L);
+        // Display the created apartment1
+        System.out.println("\n=== Created Apartment1 ===");
+        System.out.println("Apartment 1: " + apartment1);
+
+        apartmentRepository.save(apartment1);
+
+        //2.1 Create 3 reviews
+        Review review1 = new Review();
+        review1.setTitle("Fantastic house for enjoy a holidays");
+        review1.setApartment(apartment1);
+
+        Review review2 = new Review();
+        review2.setTitle("Lovely house in the middle of Barcelona town");
+        review2.setApartment(apartment1);
+
+        Review review3 = new Review();
+        review3.setTitle("Great value and lovely host");
+        review3.setApartment(apartment1);
+
+        //2.2 Display the created reviews
+        System.out.println("\n=== Created Reviews ===");
+        System.out.println("Review 1: " + review1);
+        System.out.println("Review 2: " + review2);
+        System.out.println("Review 3: " + review3);
+
+        //2.3 save 3 reviews into reviewRepository
+        reviewRepository.save(review1);
+        reviewRepository.save(review2);
+        reviewRepository.save(review3);
+
+        apartment1.getReviews().add(review1);
+        apartment1.getReviews().add(review2);
+        apartment1.getReviews().add(review3);
+
+        // !!! SOLO guardas el apartment
+        apartmentRepository.save(apartment1);
+
+        // 3. Recuperar el apartamento desde BD
+        Apartment apartmentFromDb = apartmentRepository.findById(apartment1.getId()).orElseThrow();
+
+        // 4 Acceder a las reviews de un apartamento (para ver las reviews from JPA- backend)
+        List<Review> reviews = apartmentFromDb.getReviews();
+
+        //5. Mostrar resultados
+        System.out.println("\n=== Reviews of Apartment ID " + apartmentFromDb.getId() + " ===");
+        reviews.forEach(System.out::println);
+
+        //6. Assertions (lo importante del test)
+        //assertEquals(3, reviews.size());
     }
 
 }
