acedefcbd501ccc591f3f65842e40a944b351e7a
nil.sanca97 <nil.sanca97@gmail.com>
2026-02-21 09:30:56 +0100

    model: create Reviewer class which extends Person. That means is a subclass with her own attributes & also inherence (hereda) attributes superClass Person.

--- ApartmentPredictor/src/main/java/com/example/apartment_predictor/model/Reviewer.java
+++ ApartmentPredictor/src/main/java/com/example/apartment_predictor/model/Reviewer.java
@@ -1,4 +1,52 @@
 package com.example.apartment_predictor.model;
 
-public class Reviewer {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity(name="Reviewer")
+@Table(name= "Reviewer_Person_Table")
+public class Reviewer extends Person {
+
+    private String reviewerType;
+    private int experienceYears;
+    private double averageRating;
+
+    //private List<Review> reviews;
+
+    //empty Constructor
+    public Reviewer() {}
+
+    // Constructor with all fields (including person attributes which inherence to reviewer class).
+    public Reviewer(String fullName, LocalDate birthDate, String email, String password, boolean isActive, String ReviewerType, int experienceYears, double averageRating) {
+        super(fullName, birthDate, email, password, isActive);
+        this.reviewerType = reviewerType;
+        this.experienceYears = experienceYears;
+        this.averageRating = averageRating;
+    }
+
+    // Getters & setters
+    public String getReviewerType() {
+        return reviewerType;
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+
+    public double getAverageRating() {
+        return averageRating;
+    }
+
+    // toString method of Reviewer model class
+
+    @Override
+    public String toString() {
+        return "Reviewer{" +
+                "reviewerType='" + reviewerType + '\'' +
+                ", experienceYears=" + experienceYears +
+                ", averageRating=" + averageRating +
+                "} " + super.toString();
+    }
 }
